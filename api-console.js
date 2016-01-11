eefdd2c90012d7e96dfc53f343df92b11c7639db
Brock Anderson <brock@bandersgeo.ca>
2016-01-11 13:27:03 -0800

    fixed a problem with links to external docs which was caused by interference between angular's locationProvider settings and swagger-ui. the solution was to avoid using angular locationProvider and $location to access the query string, but to use another technique

--- api-console.js
+++ api-console.js
@@ -1,45 +1,10 @@
 (function(){
 
+angular.module('ApiConsole', [])
 
-angular.module('ApiConsole', ['ngSanitize'])
+.controller('ApiConsoleCtrl', ['$scope', function($scope) {
 
-.config(function($locationProvider) {
-    // use the HTML5 History API
-    $locationProvider.html5Mode(true);
-})
-
-// This service reads data from the query string into an object.
-.service('QueryStringService', function ($location) {
-
-    this.getParams = function() {
-        var qs = $location.search();
-        //$location.url($location.path()).replace();
-        return qs;
-    };
-})
-
-.controller('ApiConsoleCtrl', ['$scope', '$location', "QueryStringService",
-    function($scope, $location, QueryStringService) {
-
-      var queryStringParams = QueryStringService.getParams();
-
-      // init form
-      $scope.isLoading = false;
-      
-      $scope.errorMsg = null;
-      if (queryStringParams["swaggerUrl"] != null && queryStringParams["swaggerUrl"] != ""){
-        $scope.swaggerUrl = queryStringParams["swaggerUrl"];
-
-        var swaggerUi = new SwaggerUi({
-          url:$scope.swaggerUrl,
-          dom_id:"swagger-ui-container"
-        });
-
-        swaggerUi.load();
-      }
-
-
-
+//do nothing
       
 }])
 
